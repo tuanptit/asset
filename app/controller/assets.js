2211e2308a2c5c4b75d00a2ad508839a9142b717
tuanptit <levantuanptit@gmail.com>
2018-06-22 16:06:18 +0700

    commit all

--- app/controller/assets.js
+++ app/controller/assets.js
@@ -716,6 +716,8 @@ var storage = multer.diskStorage({ //multers disk storage settings
     destination: function (req, file, cb) {
         cb(null, 'uploads/')
     },
+
+
     filename: function (req, file, cb) {
         var datetimestamp = Date.now();
         cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1])
